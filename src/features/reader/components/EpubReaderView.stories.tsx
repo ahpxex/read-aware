ecaa0d91ed6e1d2df618fa10a34d6cfc567f79ca
破晓 <AHpx@yandex.com>
2026-03-12 00:46:08 +0800

    Add Storybook stories for EPUB reader view

---
+++ src/features/reader/components/EpubReaderView.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { EpubReaderView } from "./EpubReaderView";
+
+const sampleBook = {
+  id: "reader-story-book",
+  title: "The Master and Margarita",
+  author: "Mikhail Bulgakov",
+  progress: 64,
+};
+
+const meta = {
+  title: "Features/Reader/EpubReaderView",
+  component: EpubReaderView,
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof EpubReaderView>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Empty: Story = {
+  args: {},
+};
+
+export const FromShelfSelection: Story = {
+  args: {
+    selectedBook: sampleBook,
+  },
+};
+
+export const WithBackAction: Story = {
+  args: {
+    selectedBook: sampleBook,
+    onBackToShelf: () => {},
+  },
+};
